e037dc890bb86538aa45defc01bf1b9e3d801c50
kiyani <bamba.kiyani@gmail.com>
2019-09-09 21:50:59 -0400

    Added personal notes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, { useState } from "react";
+
+// Personal note : the useState hook is imported from the react library
+
+import React, { useState } from "react"; 
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
+
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   
+
+
   const [lionsScore, newLionsScore] = useState(0);
   const [tigersScore, newTigersScore] = useState(0);
 
@@ -17,6 +23,8 @@ function App() {
             <h2 className="home__name">Lions</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
+
+              {/* Personal note: Here, we're rendering a component. Conceptually, components are like JS functions. They accept arbitrary inputs (called “props”) and return React elements describing what should appear on the screen. .  */}
 
             <div className="home__score"> { lionsScore } </div>
           </div>
@@ -35,6 +43,7 @@ function App() {
           <button onClick = {() => newLionsScore(lionsScore + 3)}className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
+          {/*  PN : onClick, which is an event listener, take in a callback function; newTigersScore.  */}
           <button onClick = {() => newTigersScore(tigersScore + 6)}className="awayButtons__touchdown">Away Touchdown</button>
           <button onClick = {() => newTigersScore(tigersScore + 3)}className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
